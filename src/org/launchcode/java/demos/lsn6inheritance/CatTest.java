30dd24c1668a0971cde866c2449d3456b361062a
Hannah Miller <hnmiller92@gmail.com>
2020-05-17 11:42:08 -0500

    Class 7 Exercise

---
+++ src/org/launchcode/java/demos/lsn6inheritance/CatTest.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.demos.lsn6inheritance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatTest {
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7,keyboardCat.getWeight(),.001);
+    }
+}
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,12 +1,22 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        Comparator coneComparator = new ConeComparator();
+
+        flavors.sort(comparator);
+        cones.sort(coneComparator);
+
+        for (Cone cone : cones) {
+            System.out.println(cone.getCost());
+        }
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
